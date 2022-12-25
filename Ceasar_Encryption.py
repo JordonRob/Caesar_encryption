67b675541aacb7fb69630ccb1a25335df54f02f8
JordonRob <jordonroberts99@yahoo.com>
2022-12-25 18:49:56 -0500

    Update Ceasar_Encryption.py
    Added new functionality.
    Now able to to choose to rerun program, or end it.

--- Ceasar_Encryption.py
+++ Ceasar_Encryption.py
@@ -4,31 +4,39 @@ alphabet = [
     'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-
-def caesar(direction, text, shift):
-    cipher_text = ""
-
-    for letter in text:
-        position = alphabet.index(letter)
-        if direction == "encode":
-            new_position = position + shift
-            if new_position > 25:
-                new_position -= 26
-        elif direction == "decode":
-            new_position = position - shift
-            if new_position < 0:
-                new_position += 26
-
-        cipher_text += alphabet[new_position]
-    print(f"Here is the {direction}d result of {text}:  {cipher_text}")
-
+def main():
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  
+  
+  def caesar(direction, text, shift):
+      cipher_text = ""
+  
+      for letter in text:
+          position = alphabet.index(letter)
+          if direction == "encode":
+              new_position = position + shift
+              if new_position > 25:
+                  new_position -= 26
+          elif direction == "decode":
+              new_position = position - shift
+              if new_position < 0:
+                  new_position += 26
+        
+          cipher_text += alphabet[new_position]
+      print(f"Here is the {direction}d result of {text}:  {cipher_text}")
+      print("")
+      status = input("Do you want to contine? Type 'Yes' or 'No':  ").lower()
+  
+      if status == "yes":
+        main()
+      else:
+        print("Goodbye")
     
+  
+  caesar(direction, text, shift)
+main()
 
-caesar(direction, text, shift)
 
